04b1ee754ba5c7f01eae9d024566af1dbb85e7cd
Kesik80 <55840881+Kesik80@users.noreply.github.com>
2026-06-15 23:15:02 +0200

    Upload 1 file(s)

--- api/github-token.js
+++ api/github-token.js
@@ -2,7 +2,7 @@
 // Проверяет пароль и возвращает GitHub токен
 // Env vars: GITUP_PASSWORD, GITHUB_TOKEN
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
